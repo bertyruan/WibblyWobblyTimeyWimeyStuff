05f6c00766ccabe4849c1a3921d51dbb1b0b7781
bertyruan <berty.ruan@gmail.com>
2013-10-06 19:03:41 -0400

    The World Clock is finally here!

---
+++ WibblyWobblyTimeyWimeyStuff/WibblyWobblyTimeyWimeyStuff/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Time_Zones;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WibblyWobblyTimeyWimeyStuff
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DisplayTime();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+        public void DisplayTime()
+        {
+            DateTime newDateTime = DateTime.Now;
+            DateTime Day = DateTime.Today;
+            Time.Text = newDateTime.ToString();
+            //Date.Text = Day.ToString();
+
+            //        TimeDate = new DateTimeClass();
+            //      this.DataContext = newDateTime;
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame != null)
+            {
+                if (this.Name == "ET")
+                {
+                    TimeZone name;
+                    //name.zone = "ET";
+
+                }
+
+
+                this.Frame.Navigate(typeof(BasicPage1));
+
+            }
+        }
+
+    }
+}
---
+++ WibblyWobblyTimeyWimeyStuff/WibblyWobblyTimeyWimeyStuff/TimeZone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Zones
+{
+    public class TimeZone
+    {
+
+
+        public static string zone;
+
+
+
+
+        private static Dictionary<string, int> zones = new Dictionary<string, int>()
+            {
+                {"ET",3},
+	            {"CT", 2},
+	            {"MT", 1},
+	            {"PT", 0}
+            };
+
+        static DateTime now = DateTime.Now;
+
+
+        static int value = zones[zone];
+
+        static TimeSpan duration = new TimeSpan(value, 0, 0);
+        static DateTime timezone1 = now.Add(duration);
+
+        static public int currentMinute = timezone1.Minute;
+        static public int currentHour = timezone1.Hour;
+        static public int currentDay = timezone1.Day;
+        static public DayOfWeek currentDayofWeek = timezone1.DayOfWeek;
+
+        static List<int> time = new List<int>(new int[] { currentMinute, currentHour, currentDay });
+
+        static List<int> RetrieveTime()
+        {
+            return time;
+        }
+
+        static DayOfWeek RetrieveDayOfWeek()
+        {
+            return currentDayofWeek;
+        }
+
+    }
+}
